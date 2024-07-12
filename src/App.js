d15c3ffc3405d90097d7fc54331104b865f388af
SumanthSV <sumanthsv04@gmail.com>
2024-07-12 22:53:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
-
+import NavBar from './components/Navbar';
+import Home from './components/Home'
+import About from './components/About'
+import Projects from './components/Projects'
+import SocialLinks from './components/SocialLinks'
+import Certificates from './components/Certificates';
+import Experience from './components/Experience';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <NavBar/>
+      <Home/>
+      <SocialLinks/>
+      <About/>
+      <Projects/>
+      <Experience/>
+      <Certificates/>
+    </>
   );
 }
 
---
+++ src/components/Certificates.jsx
@@ -0,0 +1,65 @@
+import React,{ useEffect } from "react"
+import c from '../assets/certificate/C_nptel.jpg'
+import java from '../assets/certificate/java_hr.jpg'
+import ps from '../assets/certificate/problem_solving.jpg'
+import gen_ai from '../assets/certificate/gen_ai.jpg'
+import AOS from 'aos'
+import 'aos/dist/aos.css'
+
+
+const Certificates = () =>{
+    useEffect(()=>{
+        AOS.init({duration:1000})
+    })
+
+    const Cert=()=>[
+        {
+            id:1,
+            src:c,
+            title:'Intro.. to c',
+            issued:"https://nptel.ac.in/noc/E_Certificate/NPTEL23CS02S4440018203121906",
+        },
+        {
+            id:2,
+            src:java,
+            title:'Java',
+            issued:"https://www.hackerrank.com/certificates/543a87a08bfd?utm_medium=email&utm_source=mail_template_1393&utm_campaign=hrc_skills_certificate",
+        },
+        {
+            id:3,
+            src:ps,
+            title:'Problem Solving',
+            issued:"https://www.hackerrank.com/certificates/db10a75ce316",
+        },
+        {
+            id:4,
+            src:gen_ai,
+            title:'Generative AI',
+            issued:"https://www.cloudskillsboost.google/public_profiles/f7f0dca3-4443-4c98-8913-3790670848a3/badges/5780754",
+        }
+    ]
+
+    return (
+        <div name="certificates" className='h-auto   bg-gradient-to-b from-black to-gray-800 w-full text-white'>
+      <div className='max-w-screen-lg p-4 mx-auto flex flex-col justify-center w-full h-full'>
+        <div className='pb-8'>
+          <h2 className='text-4xl font-bold inline border-b-4 border-gray-400 '>Certificates</h2>
+          <p className='py-6'> I am certified in following programming languages  and technologies.</p>
+        </div>
+        <div   className='grid sm:grid-cols-2 md:grid-cols-2 mb-20 gap-16 px-12 sm:px-0 '>
+          {
+            Cert().map(({ id, src, title, issued}) => (
+
+              <div data-aos="fade-in" data-aos-duration="15000" key={id} className=''> <div className='relative overflow-hidden'> <img src={src} alt="Loading" className='rounded-md duration-200 hover:scale-105 w-full' /> <div className='absolute bottom-0 w-full h-1/2 bg-gradient-to-t from-green-950 to-green-20 opacity-0.7'></div> <div className=' absolute bottom-10 left-2.5 w-full  text-white'> <p className='text-left text-xl'>{title}</p> </div> </div> <div className='flex items-center justify-end absolute bottom-0 right-0 w-full bg-gradient-to-t from-green-600 to-transparent rounded-lg'> <button className='px-6 py-3 m-4 duration-200 hover:scale-105 bg-gradient-to-l from-gray-700 to-blue-900 rounded-md'> <a target='_blank' href={issued}>View</a> </button> </div> </div>
+
+
+            ))
+          }
+        </div>
+      </div>
+    </div>
+    )
+
+}
+
+export default Certificates
